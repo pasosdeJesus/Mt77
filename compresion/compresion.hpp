f5a24b8b40ddf4b039d4b9174fac0395e21a35c0
dhasane <danihas@live.com>
2021-07-22 00:25:42 -0500

    Cambios compresion
    Ahora es mas evidente como agregar varias tendencias a un arbol
    huffman. Se pueden agregar de un archivo o un mapa. Se podria agregar
    que tambien se agregue de una cadena, en este momento solo se tiene por
    constructor, o convertir una cadena a un mapa y usar agregar mapa.
    
    Se puede agregar la cantidad de fuentes que sea, mientras no se llame
    compresion o descompresion. Una vez se llame una de estas funciones,
    se "bloquea" el arbol y ya no se pueden hacer cambios adicionales.

--- compresion/compresion.hpp
+++ compresion/compresion.hpp
@@ -17,8 +17,7 @@
 // guarde mas de un unico caracter
 #define SEPARADOR "\\\\"
 
-class nodo_arbol_huffman {
-public:
+struct nodo_arbol_huffman {
     char valor;
     std::shared_ptr<nodo_arbol_huffman>  hijo_i;
     std::shared_ptr<nodo_arbol_huffman>  hijo_d;
@@ -45,6 +44,17 @@ public:
 
 class Arbol_huffman {
 
+    // identifica si hay cambios de la tendencia
+    bool modificado;
+
+    // evitar que aun si se hacen cambios, volver a generar el arbol.
+    // Esto es importante en caso de que ya se haya empezado a
+    // escribir en disco, ya que lo que se guarde quedaria comprimido
+    // con arboles distintos, haciendo dificil la descompresion.
+    // Se tiene en comprimir y descomprimir, para asegurar que se use
+    // siempre el mismo arbol en estas funciones.
+    bool bloquear;
+
     // nodo inicial del arbol
     std::shared_ptr<nodo_arbol_huffman> raiz;
     // caracter con su respectivo codigo binario (como un string)
@@ -52,62 +62,71 @@ class Arbol_huffman {
     // tendencia de cada caracter
     std::map<char, int> tendencias;
 
-    void _conseguirCodigos(std::shared_ptr<nodo_arbol_huffman> nah, std::string codigo);
+    // consigue los codigos binarios para cada caracter
+    void generarCodigos(std::shared_ptr<nodo_arbol_huffman> nah, std::string codigo);
 
     void _imprimirPreOrden(std::shared_ptr<nodo_arbol_huffman> nah, int depth);
 
     // construye un arbol de huffman con base a un mapa de tendencias
     // realiza el trabajo principal
-    void construirArbol(std::map<char, int> tendencia, std::string archivo);
+    void construirArbol();
 
     std::string _toString(std::shared_ptr<nodo_arbol_huffman> nah);
 
-    // consigue los codigos binarios para cada caracter
-    void conseguirCodigos();
+    std::string binarioACadena(std::string codigo);
 
 public:
 
-    std::string binarioACadena(std::string codigo);
-
+// no construyen arbol ////////////////////////////////////////////////////////
     // construye un arbol vacio
     Arbol_huffman();
 
     // construye un arbol de huffman con base a una cadena y un archivo
     // en el que se haya guardado un arbol de huffman
     // iniciador, llama a construirArbol
-    Arbol_huffman(std::string cadena, std::string archivo="");
+    Arbol_huffman(std::string cadena);
 
     // construye un arbol de huffman con base a un mapa de tendencias
     // y un archivo en el que se haya guardado un arbol de huffman
     // iniciador, llama a construirArbol
-    Arbol_huffman(std::map<char, int> tendencia, std::string archivo="");
+    void sumarTendencia(std::map<char, int> tendencia);
+
+    void sumarArchivo(std::string archivo);
 
     // retorna el mapa de tendencias del arbol de huffman
     std::map<char, int> conseguirTendencia();
+
+    // guarda el arbol de huffman en un archivo con nombre "nombre"
+    void guardar(std::string nombre);
+
+// construyen arbol ///////////////////////////////////////////////////////////
+
+    // funciones bloqueantes ////////////////////////////
+
+    // comprime la cadena que se le pasa
+    std::string comprimir(std::string cadena);
 
     // descomprime la cadena en binCodigo y la retorna
     // iniciador, llama a _descomprimir
     std::string descomprimir(std::string binCodigo);
 
+    // funciones no bloqueantes /////////////////////////
+
+    // retorna true en caso de estar vacio el arbol
+    bool vacio();
+
     // imprime los nodos del arbol en pre orden
     // iniciador, llama a _imprimirPreOrden
     void imprimirPreOrden();
-
-    // comprime la cadena que se le pasa
-    std::string comprimir(std::string cadena);
 
     // convierte el arbol a una cadena
     // iniciador, llama a _toString
     std::string toString();
 
-    // guarda el arbol de huffman en un archivo con nombre "nombre"
-    void guardar(std::string nombre);
+// funciones estaticas ////////////////////////////////////////////////////////
 
     // carga un arbol de huffman en un archivo con nombre "nombre"
     static std::map<char, int> cargar(std::string nombre);
-
-    // retorna true en caso de estar vacio el arbol
-    bool vacio();
 
     // agrega el segundo mapa al primer mapa
     static void sumarMapas(std::map<char, int> &map1, std::map<char, int> map2);
